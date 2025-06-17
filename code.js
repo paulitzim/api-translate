c54f8ff16082a46edc5ad4a4b84f91cac58beaf1
paulitzim <paulaserman@gmail.com>
2025-06-17 15:05:45 -0400

    ui

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-figma.showUI(__html__, { width: 300, height: 500 });
+figma.showUI(__html__, { width: 300, height: 550 });
 
 figma.ui.onmessage = async (msg) => {
   if (msg.type === "translate-all") {
